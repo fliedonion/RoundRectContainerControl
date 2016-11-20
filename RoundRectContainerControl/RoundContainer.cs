c03664eb678ef610e45f5b2e377d8202348b8826
Takahiro KITAHARA <fliedonion@users.noreply.github.com>
2016-11-21 03:20:20 +0900

    Add first version

---
+++ RoundRectContainerControl/RoundContainer.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing;
+using System.Data;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Case_of_t.net.WinForms.Controls.RoundRectContainerControl {
+
+    [Designer("System.Windows.Forms.Design.ParentControlDesigner, System.Design", typeof(IDesigner))]
+    [System.ComponentModel.DesignerCategory("Code")]
+    public partial class RoundRectContainer : UserControl {
+        public RoundRectContainer() {
+            InitializeComponent();
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            SetStyle(ControlStyles.Selectable, false);
+            SetStyle(ControlStyles.ResizeRedraw, true);
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.ContainerControl, true);
+            base.BackColor = Color.Transparent;
+        }
+
+        private int borderWidth = 1;
+        private int dropShadowOffset = 4;
+        private DropShadowDirections dropShadowDirection = DropShadowDirections.BottomRight;
+        private Color borderColor = SystemColors.ActiveBorder;
+        private Color borderFillColor = Color.Transparent;
+        private Color dropShadowColor = SystemColors.ActiveBorder;
+
+        protected const int MaxRaduis = 500;
+        protected int cornerRadius = 4;
+
+        private bool topLeft = true;
+        private bool topRight = true;
+        private bool bottomRight = true;
+        private bool bottomLeft = true;
+
+
+
+
+        public enum DropShadowDirections {
+            None = 0,
+            TopLeft = 1,
+            TopRight = 2,
+            BottomRight = 4,
+            BottomLeft = 8,
+        }
+
+        [Category("Appearance")]
+        [DefaultValue(4)]
+        public int DropShadowOffset {
+            get { return dropShadowOffset; }
+            set {
+                dropShadowOffset = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [Description("現在は右下のみサポートしています。This version only supports BottomRight")]
+        [DefaultValue(DropShadowDirections.BottomRight)]
+        public DropShadowDirections DropShadowDirection {
+            get { return dropShadowDirection; }
+            set {
+                dropShadowDirection = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        public Color BorderFillColor {
+            get { return borderFillColor; }
+            set {
+                borderFillColor = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        public Color DropShadowColor {
+            get { return dropShadowColor; }
+            set {
+                dropShadowColor = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [DefaultValue(true)]
+        public bool CornerRoundTopLeft {
+            get { return topLeft; }
+            set {
+                topLeft = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [DefaultValue(true)]
+        public bool CornerRoundTopRight {
+            get { return topRight; }
+            set {
+                topRight = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [DefaultValue(true)]
+        public bool CornerRoundBottomRight {
+            get { return bottomRight; }
+            set {
+                bottomRight = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        [DefaultValue(true)]
+        public bool CornerRoundBottomLeft {
+            get { return bottomLeft; }
+            set {
+                bottomLeft = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        public int BorderWidth {
+            get { return borderWidth; }
+            set {
+                borderWidth = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        public Color BorderColor {
+            get { return borderColor; }
+            set {
+                borderColor = value;
+                Invalidate();
+            }
+        }
+
+        [Category("Appearance")]
+        public int CornerRadius {
+            get { return cornerRadius; }
+            set {
+                var shadowSize = dropShadowOffset;
+                if (dropShadowColor == Color.Transparent || dropShadowDirection == DropShadowDirections.None) {
+                    shadowSize = 0;
+                }
+
+                var max = (Width > Height ? Height + shadowSize : Width + shadowSize) / 3;
+                cornerRadius = value > max ? max : value;
+                Invalidate();
+            }
+        }
+
+        [Browsable(false)]
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override Color BackColor {
+            get { return base.BackColor; }
+            set { base.BackColor = Color.Transparent; } 
+        }
+
+
+        protected override void OnSizeChanged(EventArgs e) {
+            CornerRadius = cornerRadius;
+            base.OnSizeChanged(e);
+        }
+
+        protected override void OnClientSizeChanged(EventArgs e) {
+            CornerRadius = cornerRadius;
+            base.OnClientSizeChanged(e);
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e) {
+            base.OnPaint(e);
+
+            var baseRect = new Rectangle(
+                BorderWidth + Padding.Left,
+                BorderWidth + Padding.Top,
+                Width - BorderWidth * 2 - (Padding.Right + Padding.Left),
+                Height - BorderWidth * 2 - (Padding.Bottom + Padding.Top));
+
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+
+            DrawDropShadow(e.Graphics, baseRect);
+            DrawBorderRect(e.Graphics, baseRect);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e) {
+            base.OnPaintBackground(e);
+        }
+
+        protected virtual void DrawDropShadow(Graphics g, Rectangle rect) {
+
+            if(DropShadowDirection == DropShadowDirections.None) return;
+            if (DropShadowColor == Color.Transparent) return;
+
+            rect.Size = new Size(rect.Size.Width - dropShadowOffset, rect.Size.Height - dropShadowOffset);
+            switch (DropShadowDirection) {
+                case DropShadowDirections.BottomLeft:
+                    rect.Location = new Point(rect.X, rect.Y + DropShadowOffset);
+                    break;
+                case DropShadowDirections.BottomRight:
+                    rect.Location = new Point(rect.X + DropShadowOffset, rect.Y + DropShadowOffset);
+                    break;
+                case DropShadowDirections.TopLeft:
+                    rect.Location = new Point(rect.X, rect.Y);
+                    break;
+                case DropShadowDirections.TopRight:
+                    rect.Location = new Point(rect.X + DropShadowOffset, rect.Y);
+                    break;
+            }
+
+            var oldCompMode = g.CompositingMode;
+            g.CompositingMode = CompositingMode.SourceOver;
+
+            using (var path = RectToGraphicPath(rect, topLeft, topRight, bottomRight, bottomLeft)) {
+                if (DropShadowColor != Color.Transparent) {
+                    if (BorderFillColor != Color.Transparent) {
+                        using (var brush = new SolidBrush(DropShadowColor)) {
+                            g.FillPath(brush, path);
+                        }
+                    }
+                    else {
+                        using (var pen = new Pen(Color.FromArgb(128, DropShadowColor), BorderWidth)) {
+                            g.DrawPath(pen, path);
+                        }
+                    }
+                }
+            }
+            g.CompositingMode =oldCompMode;
+        }
+
+        protected virtual void DrawBorderRect(Graphics g, Rectangle rect) {
+
+            if (DropShadowDirection != DropShadowDirections.None) {
+                rect.Size = new Size(rect.Size.Width - dropShadowOffset, rect.Size.Height - dropShadowOffset);
+
+                switch (DropShadowDirection) {
+                    case DropShadowDirections.BottomLeft:
+                        rect.Location = new Point(rect.X + DropShadowOffset, rect.Y);
+                        break;
+                    case DropShadowDirections.BottomRight:
+                        rect.Location = new Point(rect.X, rect.Y);
+                        break;
+                    case DropShadowDirections.TopLeft:
+                        rect.Location = new Point(rect.X + DropShadowOffset, rect.Y + DropShadowOffset);
+                        break;
+                    case DropShadowDirections.TopRight:
+                        rect.Location = new Point(rect.X, rect.Y + DropShadowOffset);
+                        break;
+                }
+            }
+
+            using (var path = RectToGraphicPath(rect, topLeft, topRight, bottomRight, bottomLeft)) {
+
+                if (BorderFillColor != Color.Transparent) {
+                    using(var brush = new SolidBrush(BorderFillColor)){
+                        g.FillPath(brush, path);    
+                    }
+                }
+
+                using(var pen = new Pen(BorderColor, BorderWidth))
+                g.DrawPath(pen, path);
+
+            }
+
+        }
+
+        protected GraphicsPath RectToGraphicPath(Rectangle rect, bool topLeft, bool topRight, bool bottomRight, bool bottomLeft) {
+            var round = CornerRadius * 2;
+            var path = new GraphicsPath();
+
+            int lt = topLeft ? round : 0;
+            int rt = topRight ? round : 0;
+            int rb = bottomRight ? round : 0;
+            int lb = bottomLeft ? round : 0;
+
+
+            path.AddLine(rect.Left + lt, rect.Top, rect.Right - rt, rect.Top);
+            if(rt > 0) path.AddArc(Rectangle.FromLTRB(rect.Right - rt, rect.Top, rect.Right, rect.Top + rt), -90, 90);
+
+            path.AddLine(rect.Right, rect.Top + rt, rect.Right, rect.Bottom - rb);
+            if(rb > 0) path.AddArc(Rectangle.FromLTRB(rect.Right - rb, rect.Bottom - rb, rect.Right, rect.Bottom), 0, 90);
+
+            path.AddLine(rect.Right - rb, rect.Bottom, rect.Left + lb, rect.Bottom);
+            if(lb > 0) path.AddArc(Rectangle.FromLTRB(rect.Left, rect.Bottom - lb, rect.Left + lb, rect.Bottom), 90, 90);
+
+            path.AddLine(rect.Left, rect.Bottom - lb, rect.Left, rect.Top + lt);
+            if(lt > 0) path.AddArc(Rectangle.FromLTRB(rect.Left, rect.Top, rect.Left + lt, rect.Top + lt), 180, 90);
+
+            path.CloseFigure();
+
+            return path;
+        }
+
+    }
+}
---
+++ SampleForm/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace RoundRectSample {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.roundRectContainer4 = new Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer();
+            this.roundRectContainer2 = new Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer();
+            this.roundRectContainer3 = new Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer();
+            this.roundRectContainer1 = new Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.roundRectContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // roundRectContainer4
+            // 
+            this.roundRectContainer4.BackColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer4.BorderColor = System.Drawing.SystemColors.Highlight;
+            this.roundRectContainer4.BorderFillColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer4.BorderWidth = 10;
+            this.roundRectContainer4.CornerRadius = 4;
+            this.roundRectContainer4.DropShadowColor = System.Drawing.Color.LightBlue;
+            this.roundRectContainer4.DropShadowOffset = 16;
+            this.roundRectContainer4.Location = new System.Drawing.Point(85, 372);
+            this.roundRectContainer4.Name = "roundRectContainer4";
+            this.roundRectContainer4.Size = new System.Drawing.Size(92, 81);
+            this.roundRectContainer4.TabIndex = 5;
+            // 
+            // roundRectContainer2
+            // 
+            this.roundRectContainer2.BackColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer2.BorderColor = System.Drawing.Color.GreenYellow;
+            this.roundRectContainer2.BorderFillColor = System.Drawing.Color.LightCyan;
+            this.roundRectContainer2.BorderWidth = 5;
+            this.roundRectContainer2.CornerRadius = 16;
+            this.roundRectContainer2.DropShadowColor = System.Drawing.Color.LightBlue;
+            this.roundRectContainer2.DropShadowOffset = 8;
+            this.roundRectContainer2.Location = new System.Drawing.Point(32, 202);
+            this.roundRectContainer2.Name = "roundRectContainer2";
+            this.roundRectContainer2.Size = new System.Drawing.Size(188, 164);
+            this.roundRectContainer2.TabIndex = 3;
+            // 
+            // roundRectContainer3
+            // 
+            this.roundRectContainer3.BackColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer3.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.roundRectContainer3.BorderFillColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer3.BorderWidth = 3;
+            this.roundRectContainer3.CornerRadius = 40;
+            this.roundRectContainer3.CornerRoundBottomRight = false;
+            this.roundRectContainer3.CornerRoundTopRight = false;
+            this.roundRectContainer3.DropShadowColor = System.Drawing.Color.LightBlue;
+            this.roundRectContainer3.DropShadowDirection = Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer.DropShadowDirections.None;
+            this.roundRectContainer3.Location = new System.Drawing.Point(348, 228);
+            this.roundRectContainer3.Name = "roundRectContainer3";
+            this.roundRectContainer3.Size = new System.Drawing.Size(150, 138);
+            this.roundRectContainer3.TabIndex = 4;
+            // 
+            // roundRectContainer1
+            // 
+            this.roundRectContainer1.BackColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer1.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.roundRectContainer1.BorderFillColor = System.Drawing.Color.Transparent;
+            this.roundRectContainer1.BorderWidth = 1;
+            this.roundRectContainer1.Controls.Add(this.checkBox1);
+            this.roundRectContainer1.CornerRadius = 4;
+            this.roundRectContainer1.DropShadowColor = System.Drawing.SystemColors.ActiveBorder;
+            this.roundRectContainer1.Location = new System.Drawing.Point(72, 40);
+            this.roundRectContainer1.Name = "roundRectContainer1";
+            this.roundRectContainer1.Size = new System.Drawing.Size(232, 146);
+            this.roundRectContainer1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(48, 63);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(115, 22);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 566);
+            this.Controls.Add(this.roundRectContainer4);
+            this.Controls.Add(this.roundRectContainer2);
+            this.Controls.Add(this.roundRectContainer3);
+            this.Controls.Add(this.roundRectContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.roundRectContainer1.ResumeLayout(false);
+            this.roundRectContainer1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer roundRectContainer1;
+        private Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer roundRectContainer2;
+        private Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer roundRectContainer3;
+        private Case_of_t.net.WinForms.Controls.RoundRectContainerControl.RoundRectContainer roundRectContainer4;
+    }
+}
+
